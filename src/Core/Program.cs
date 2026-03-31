c2390bf0b36add7f505f2287193b14b1de52696f
Luciano Spiguel <46658141+lspiguel@users.noreply.github.com>
2026-03-31 19:18:31 -0300

    Starting with code.

---
+++ src/Core/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SystemFitnessHelper.Core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
